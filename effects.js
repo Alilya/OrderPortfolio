997c7234d3928a1a6cc0abfb00b876fcdf5ddd35
Alilya <alinakolesnikova0101@gmail.com>
2023-04-26 12:38:32 +0300

    База

---
+++ effects.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector('.parallax');
+const layers = document.querySelectorAll('.parallax__layer');
+
+const handleParallax = (evt) => {
+ //размер области просмотра
+  const parallaxLeftOffset = wrapper.getBoundingClientRect().left;
+  const parallaxTopOffset = wrapper.getBoundingClientRect().top;
+
+  // координаты центра экрана
+  const coordX = evt.clientX - parallaxLeftOffset - 0.5 * wrapper.offsetWidth;
+  const coordY = evt.clientY - parallaxTopOffset - 0.5 *  wrapper.offsetHeight;
+    
+  layers.forEach((layer)=>{
+    const layerSpeed = layer.dataset.speed;
+    const x = - (coordX * layerSpeed).toFixed(5);
+    const y = - (coordY * layerSpeed).toFixed(5);
+    layer.setAttribute('style', `transform: translate(${x}px, ${y}px);`)
+  });
+};
+
+const reset = () => {
+  layers.forEach((layer)=>{
+      layer.removeAttribute('style');
+  });
+}
+ 
+wrapper.addEventListener('mousemove', handleParallax);
+wrapper.addEventListener('mouseout', reset);
